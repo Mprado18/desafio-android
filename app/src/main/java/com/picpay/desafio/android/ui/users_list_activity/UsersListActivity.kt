8d6d76d74bfdfadb6636d272d62a4c740af02186
Marcos Prado <marcosprprado@yahoo.com.br>
2022-04-14 16:52:53 -0300

    refact: adjusted layouts, created dimen and change synthetics/find view by id to view binding

--- app/src/main/java/com/picpay/desafio/android/ui/users_list_activity/UsersListActivity.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/users_list_activity/UsersListActivity.kt
@@ -3,13 +3,12 @@ package com.picpay.desafio.android.ui.users_list_activity
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
-import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.picpay.desafio.android.R
 import com.picpay.desafio.android.api.model.User
+import com.picpay.desafio.android.databinding.ActivityUsersListBinding
 import com.picpay.desafio.android.interactor.GetUsersInteractor
 import com.picpay.desafio.android.ui.users_list_activity.recyclerview.UserListAdapter
 import com.picpay.desafio.android.ui.users_list_activity.viewmodel.UsersCommand
@@ -20,58 +19,65 @@ import org.koin.core.parameter.parametersOf
 
 class UsersListActivity : AppCompatActivity() {
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var progressBar: ProgressBar
-    private lateinit var adapter: UserListAdapter
+    private lateinit var binding: ActivityUsersListBinding
+    private lateinit var userListAdapter: UserListAdapter
 
     private val viewModel: UsersViewModel by viewModel {
         parametersOf(GetUsersInteractor())
     }
 
-    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_users_list)
+        binding = ActivityUsersListBinding.inflate(layoutInflater)
+        val viewBinding = binding.root
+        setContentView(viewBinding)
 
         setupRecyclerView()
         setupProgressBar()
+        observe()
 
+        viewModel.getUsers()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun observe() {
         viewModel.usersMutableLiveData.observe(this, Observer { users ->
             users?.let {
-                adapter.notifyDataSetChanged()
+                userListAdapter.notifyDataSetChanged()
                 when (it) {
                     is UsersCommand.RenderUsers -> renderUsers(it.userList)
                     is UsersCommand.ShowRequestError -> showRequestError()
                 }
             }
         })
-
-        viewModel.getUsers()
     }
 
-    private fun setupProgressBar() {
-        progressBar = findViewById(R.id.user_list_progress_bar)
-        progressBar.visibility = View.VISIBLE
+    private fun setupProgressBar() = with(binding) {
+        userListProgressBar.visibility = View.VISIBLE
     }
 
-    private fun setupRecyclerView() {
-        adapter = UserListAdapter()
+    private fun setupRecyclerView() = with(binding) {
+        userListAdapter = UserListAdapter()
 
-        recyclerView = findViewById(R.id.recyclerView)
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        with(recyclerView) {
+            adapter = userListAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
     }
 
-    private fun renderUsers(usersAdd: List<User>) {
-        progressBar.visibility = View.GONE
-        adapter.users = usersAdd
+    private fun renderUsers(usersList: List<User>) = with(binding) {
+        userListProgressBar.visibility = View.GONE
+        userListAdapter.users = usersList
     }
 
-    private fun showRequestError() {
-        progressBar.visibility = View.GONE
+    private fun showRequestError() = with(binding) {
+        userListProgressBar.visibility = View.GONE
         recyclerView.visibility = View.GONE
 
-        ToastNotification().notification(this, getString(R.string.error))
+        ToastNotification().notification(
+            this@UsersListActivity,
+            getString(R.string.error)
+        )
     }
 
 }
--- app/src/main/java/com/picpay/desafio/android/ui/users_list_activity/recyclerview/UserListItemViewHolder.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/users_list_activity/recyclerview/UserListItemViewHolder.kt
@@ -4,28 +4,30 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.picpay.desafio.android.R
 import com.picpay.desafio.android.api.model.User
+import com.picpay.desafio.android.databinding.ListItemUserBinding
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.list_item_user.view.*
 
 class UserListItemViewHolder(
     itemView: View
 ) : RecyclerView.ViewHolder(itemView) {
 
-    fun bind(user: User) {
-        itemView.name.text = user.name
-        itemView.username.text = user.username
-        itemView.progressBar.visibility = View.VISIBLE
+    private val binding = ListItemUserBinding.bind(itemView)
+
+    fun bind(user: User) = with(binding) {
+        name.text = user.name
+        username.text = user.username
+        progressBar.visibility = View.VISIBLE
         Picasso.get()
             .load(user.img)
             .error(R.drawable.ic_round_account_circle)
-            .into(itemView.picture, object : Callback {
+            .into(picture, object : Callback {
                 override fun onSuccess() {
-                    itemView.progressBar.visibility = View.GONE
+                    progressBar.visibility = View.GONE
                 }
 
                 override fun onError(e: Exception?) {
-                    itemView.progressBar.visibility = View.GONE
+                    progressBar.visibility = View.GONE
                 }
             })
     }
